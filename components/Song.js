a1eecf68ca408e79a7e0f72d97d796c0c9900b2f
Andy Williams <andy@nonissue.org>
2020-01-25 02:27:30 -0700

    Cleanup

--- components/Song.js
+++ components/Song.js
@@ -4,7 +4,9 @@ import styles from './Song.module.css';
 const Song = ({ title, artist, album }) => (
   <div className={styles.item}>
     <div>
-      {artist} <br /> {title} <br /> {album}
+      <div className={styles.artist}>{artist}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.album}>{album}</div>
     </div>
   </div>
 );
