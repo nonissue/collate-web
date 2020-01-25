44bdd2c4486ce6b8589b7430eb1450c21c82672f
Andy Williams <andy@nonissue.org>
2020-01-25 02:35:39 -0700

    bump

--- pages/songs.js
+++ pages/songs.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import styles from './Songs.module.css';
+import styles from './songs.module.css';
 import Song from '../components/Song';
 import Select from '../components/Select';
 import { songs } from '../mocks/songs';
