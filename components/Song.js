724b32d3f48ae28ab001b4b1953f4efb6d0a1604
Andy Williams <andy@nonissue.org>
2020-01-26 03:01:55 -0700

    imported lib/slugify in song

--- components/Song.js
+++ components/Song.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './Song.module.css';
-
-function slugify(string) {
-  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
-  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
-  const p = new RegExp(a.split('').join('|'), 'g');
-
-  return string
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, ''); // Trim - from end of text
-}
+import { slugify } from '../lib/slugify';
 
 const Song = ({ title, artist, album, id }) => (
   <div className={styles.wrapper}>
