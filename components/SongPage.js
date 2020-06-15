7aa6444ca3726ecf849626242e1ea01eaf0e3df7
nonissue <andy@nonissue.org>
2020-06-15 16:32:39 -0600

    Tweak SongPage styling, add play button

--- components/SongPage.js
+++ components/SongPage.js
@@ -1,21 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { ArrowRightMinor, ArrowLeftMinor } from '@shopify/polaris-icons';
+import {
+  ArrowRightMinor,
+  ArrowLeftMinor,
+  PlayCircleMajorMonotone,
+  PauseCircleMajorMonotone,
+} from '@shopify/polaris-icons';
 import styles from './SongPage.module.css';
 import { songs } from '../mocks/songs';
 import { slugify } from '../lib/slugify';
 
 export const SongPage = ({ id, title, artist, album, tags, genre }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const index = songs.findIndex(s => s.id === id);
   const next = songs[index + 1];
   const prev = songs[index - 1];
-  console.log(index);
-  console.log(id);
 
   return (
     <div className={styles.wrapper}>
       <div>
+        <div className={`${styles.controls} ${styles.nextprev}`}>
+          {prev ? (
+            <div className={styles['controls-button']}>
+              <Link href={`/songs?id=${prev.id}`} as={`/songs/${slugify(prev.title)}`}>
+                <a>
+                  <ArrowLeftMinor viewBox="0 0 20 20" />
+                </a>
+              </Link>
+
+              <div className={styles.backdrop} />
+            </div>
+          ) : (
+            <div className={`${styles['controls-button']} ${styles['controls-disabled']}`}>
+              <a>
+                <ArrowLeftMinor viewBox="0 0 20 20" />
+              </a>
+
+              <div className={styles.backdrop} />
+            </div>
+          )}
+          {next ? (
+            <div className={styles['controls-button']}>
+              <Link href={`/songs?id=${next.id}`} as={`/songs/${slugify(next.title)}`}>
+                <a>
+                  <ArrowRightMinor viewBox="0 0 20 20" />
+                </a>
+              </Link>
+
+              <div className={styles.backdrop} />
+            </div>
+          ) : (
+            <div className={`${styles['controls-button']} ${styles['controls-disabled']}`}>
+              <a>
+                <ArrowRightMinor viewBox="0 0 20 20" />
+              </a>
+
+              <div className={styles.backdrop} />
+            </div>
+          )}
+        </div>
         <div className={styles.artist}>
           {artist.first} {artist.last}
         </div>
@@ -24,29 +69,21 @@ export const SongPage = ({ id, title, artist, album, tags, genre }) => {
         {/* <div className={styles.tags}>{tags.map(t => `${t} `)}</div> */}
         {/* <div className={styles.genre}>{genre}</div> */}
       </div>
-      <div className={styles.nextprev}>
-        {prev && (
-          <div className={styles['nextprev-button']}>
-            <Link href={`/songs?id=${prev.id}`} as={`/songs/${slugify(prev.title)}`}>
-              <a>
-                <ArrowLeftMinor viewBox="0 0 20 20" />
-              </a>
-            </Link>
-
-            <div className={styles.backdrop} />
-          </div>
-        )}
-        {next && (
-          <div className={styles['nextprev-button']}>
-            <Link href={`/songs?id=${next.id}`} as={`/songs/${slugify(next.title)}`}>
-              <a>
-                <ArrowRightMinor viewBox="0 0 20 20" />
-              </a>
-            </Link>
-
-            <div className={styles.backdrop} />
-          </div>
-        )}
+      <div className={`${styles.controls} ${styles.play}`}>
+        <button
+          type="button"
+          className={`${styles['controls-button']}`}
+          onClick={() => setIsPlaying(!isPlaying)}
+        >
+          <a>
+            {isPlaying ? (
+              <PauseCircleMajorMonotone viewBox="0 0 20 20" />
+            ) : (
+              <PlayCircleMajorMonotone viewBox="0 0 20 20" />
+            )}
+          </a>
+          <div className={styles.backdrop} />
+        </button>
       </div>
     </div>
   );
